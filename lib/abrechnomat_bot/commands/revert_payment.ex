93c481d7ad18a47501ed9f2582759be116860875
Peter Haßler <pet.hassler@gmail.com>
2020-08-09 11:20:53 +0200

    Add error handling to revert payment

--- lib/abrechnomat_bot/commands/revert_payment.ex
+++ lib/abrechnomat_bot/commands/revert_payment.ex
@@ -2,18 +2,15 @@ defmodule AbrechnomatBot.Commands.RevertPayment do
   require Amnesia
   require Amnesia.Helper
   alias AbrechnomatBot.Database.{Bill, Payment}
+  import Phoenix.HTML
 
   def command(args) do
     args
     |> parse
-    |> case do
-      # TODO
-      :error -> nil
-      parsed -> execute(parsed)
-    end
+    |> execute
   end
 
-  def execute({payment_id, chat_id, message_id}) do
+  def execute({:ok, {payment_id, chat_id, message_id}}) do
     Amnesia.transaction do
       case Bill.find_by_chat(chat_id) do
         nil ->
@@ -28,6 +25,23 @@ defmodule AbrechnomatBot.Commands.RevertPayment do
     end
   end
 
+  def execute({:error, _, {chat_id, message_id}}) do
+    usage()
+    |> reply(chat_id, message_id)
+  end
+
+  defp usage do
+    cmd_usage = "/revert_payment [id]"
+    text = "Use ID from bot's response to /add_payment"
+    
+    ~E"""
+    <code><%= cmd_usage %></code>
+    <%= text %>
+    """
+    |> safe_to_string
+  end
+
+
   defp parse(
          {text, %Nadia.Model.Update{message: %{message_id: message_id, chat: %{id: chat_id}}}}
        ) do
@@ -35,12 +49,13 @@ defmodule AbrechnomatBot.Commands.RevertPayment do
     |> String.trim()
     |> Integer.parse()
     |> case do
-      :error -> :error
-      {payment_id, _} -> {payment_id, chat_id, message_id}
+      :error -> {:error, :invalid_payment_id, {chat_id, message_id}}
+      {payment_id, _} -> {:ok, {payment_id, chat_id, message_id}}
     end
   end
 
+
   defp reply(text, chat_id, message_id) do
-    Nadia.send_message(chat_id, text, reply_to_message_id: message_id)
+    Nadia.send_message(chat_id, text, reply_to_message_id: message_id, parse_mode: "HTML")
   end
 end
