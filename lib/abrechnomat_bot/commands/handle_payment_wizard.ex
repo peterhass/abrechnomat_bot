61b899184a975623ebe9e49710c007714c297dd2
Peter Haßler <peter@hassler.in>
2024-11-17 20:12:41 +0100

    refactor: simplify command handling

--- lib/abrechnomat_bot/commands/handle_payment_wizard.ex
+++ lib/abrechnomat_bot/commands/handle_payment_wizard.ex
@@ -3,11 +3,26 @@ defmodule AbrechnomatBot.Commands.HandlePaymentWizard do
   require Amnesia.Helper
   alias AbrechnomatBot.Commands.MessageContextStore
 
-  def command(args) do
-    args
-    |> parse
-    |> tap(fn arg -> IO.inspect(arg) end)
-    |> execute
+  def command(
+        {_,
+         %Nadia.Model.Update{
+           message: %{
+             message_id: message_id,
+             chat: %{id: chat_id}
+           }
+         }}
+      ) do
+    {:ok, %{message_id: response_message_id}} =
+      Nadia.send_message(chat_id, "Amount",
+        reply_to_message_id: message_id,
+        reply_markup: %{
+          force_reply: true,
+          input_field_placeholder: "100.00 EUR",
+          selective: true
+        }
+      )
+
+    MessageContextStore.set_value(response_message_id, __MODULE__, :amount)
   end
 
   def reply_command(
@@ -116,37 +131,5 @@ defmodule AbrechnomatBot.Commands.HandlePaymentWizard do
     Nadia.send_message(chat_id, "Amount: #{amount}, Split: #{text}",
       reply_to_message_id: message_id
     )
-  end
-
-  def parse({
-        _,
-        %Nadia.Model.Update{
-          message: %{
-            message_id: message_id,
-            chat: %{id: chat_id}
-          }
-        }
-      }) do
-    {:start, {chat_id, message_id}}
-  end
-
-  def execute(
-        {:start,
-         {
-           chat_id,
-           message_id
-         }}
-      ) do
-    {:ok, %{message_id: response_message_id}} =
-      Nadia.send_message(chat_id, "Amount",
-        reply_to_message_id: message_id,
-        reply_markup: %{
-          force_reply: true,
-          input_field_placeholder: "100.00 EUR",
-          selective: true
-        }
-      )
-
-    MessageContextStore.set_value(response_message_id, __MODULE__, :amount)
   end
 end
